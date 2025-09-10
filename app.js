46eba06b4049ef06ac199d98026cedafe1a6cc61
ruslankozenko <kozenko.r@seznam.cz>
2025-09-10 17:27:10 +0200

    js add

--- app.js
+++ app.js
@@ -6,11 +6,13 @@ const backdrop = document.querySelector('#backdrop')
 content.addEventListener('click', openCard)
 backdrop.addEventListener('click', closeModal)
 
-// modal.classList.add('open')
-
-// setTimeout(function () {
-//     modal.classList.remove('open')
-// }, 2000)
+const technologies =[
+    {title: 'HTML', description: 'HTML Text', type: 'html', done: true},
+    {title: 'CSS', description: 'CSS Text', type: 'css', done: true},
+    {title: 'JavaScript', description: 'JavaScript Text', type: 'js', done: false},
+    {title: 'Git', description: 'Git Text', type: 'git', done: false},
+    {title: 'React', description: 'React Text', type: 'react', done: false}
+]
 
 function openCard() {
     modal.classList.add('open')
@@ -19,4 +21,32 @@ function openCard() {
 function closeModal() {
     modal.classList.remove('open')
 }
-    
+
+function init() {
+    if (technologies.length === 0) {
+    content.innerHTML = '<p class="empty">No technologies added. Add the first one.</p>'
+} else {
+     let html = ''
+for (let i = 0; i < technologies.length; i++) {
+    const tech = technologies[i]
+    html += toCard(tech)
+}
+content.innerHTML = html
+}
+}
+
+function toCard(tech) {
+    let doneClass = ''
+
+    if (tech.done === true) {
+        doneClass = 'done'
+    } else {
+        doneClass = ''
+    }
+
+    return ` <div class="card ${doneClass}">
+    <h3>${tech.title}</h3>
+    </div> `
+}
+
+init()    
